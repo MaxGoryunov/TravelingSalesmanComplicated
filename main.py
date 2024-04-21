44b5aee8f115226976a5077e43bb6dcbf84280eb
eagerbeaver04 <mike.markov04@gmail.com>
2024-04-21 16:03:22 +0300

    update with troubles

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-import matplotlib.pyplot as plt
 import math as mt
 from datetime import datetime
 
@@ -8,10 +7,10 @@ def distance(x1, y1, x2, y2):
     return mt.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 
-M = 2
-N = 1
-mm = 1
-nn = 1
+M = 8
+N = 8
+mm = 5
+nn = 5
 INF = 2 ** 31 - 1
 
 M_TYPE = 0
@@ -29,7 +28,7 @@ for i in range(n):
         count = count - 1
     else:
         points[i] = (random.randint(1, 10), random.randint(1, 10), N_TYPE)
-print(points)
+
 input_matrix = []
 for i, vi in points.items():
     m1 = []
@@ -45,55 +44,65 @@ for i, vi in points.items():
 # -----------------------------------------------------------------
 def tsp(input_matrix):
     n = len(input_matrix)
-    # Генерация служебных массивов
-    s = (1 << (n - 1)) - 1
-    path = [0] * s
-    local_sum = [0] * s
+    results = []
+    for num in range(n - 1):
+        current_points = points.copy()
+        current_points[0] = points[num]
+        current_points[num] = points[0]
+        # Генерация служебных массивов
+        s = (1 << (n - 1)) - 1
+        path = [0] * s
+        local_sum = [0] * s
 
-    for i in range(s):
-        path[i] = [0] * (n - 1)
-        local_sum[i] = [-1] * (n - 1)
-    m = [n - 1, input_matrix.copy(), path, local_sum, mm, nn]
+        for i in range(s):
+            path[i] = [0] * (n - 1)
+            local_sum[i] = [-1] * (n - 1)
+        m = [n - 1, input_matrix.copy(), path, local_sum, mm, nn]
+        sum_path = INF
+        for i in range(m[0]):
+            index = 1 << i
+            if s & index != 0:
+                if current_points[i][2] == M_TYPE:
+                    m[4] -= 1#если сдвиг и возврат здесь делать на 2,чтобы городов стало на 1 меньше, то код падает(
+                else:
+                    m[5] -= 1
+                if m[4] > 0 or m[5] > 0:
+                    sum_temp = tsp_next(m, s ^ index, i,current_points) + m[1][i + 1][num]#delete При необходимости
+                    if sum_temp < sum_path:
+                        sum_path = sum_temp
+                        m[2][0][0] = i + 1
+                if current_points[i][2] == M_TYPE:
+                    m[4] += 1
+                else:
+                    m[5] += 1
 
-    sum_path = INF
-    for i in range(m[0]):
-        index = 1 << i
-        if s & index != 0:
-            if points[i][2] == M_TYPE:
-                m[4] -= 1
-            else:
-                m[5] -= 1
-            if m[4] > 0 or m[5] > 0:
-                sum_temp = tsp_next(m, s ^ index, i) + m[1][i + 1][0]
-                if sum_temp < sum_path:
-                    sum_path = sum_temp
-                    m[2][0][0] = i + 1
-            if points[i][2] == M_TYPE:
-                m[4] += 1
-            else:
-                m[5] += 1
+        m[3][0][0] = sum_path
 
-    m[3][0][0] = sum_path
-
-    # Вывод оптимального пути
-    res = []
-    init_point = int(path[0][0])
-    res.append(init_point)
-    s = ((1 << m[0]) - 1) ^ (1 << init_point - 1)
-    counter = 0
-    for i in range(1, m[0]):
-        init_point = int(path[s][init_point - 1])
+        # Вывод оптимального пути
+        res = []
+        init_point = int(path[0][0])
         res.append(init_point)
-        s = s ^ (1 << init_point - 1)
-        counter += 1
-        if counter == mm + nn:
-            break
-    res.append(0)
-    return [sum_path, res]
+        s = ((1 << m[0]) - 1) ^ (1 << init_point - 1)
+        counter = 0
+        for i in range(1, m[0]):
+            counter += 1
+            if counter == mm + nn:
+                break
+            init_point = int(path[s][init_point - 1])
+            res.append(init_point)
+            s = s ^ (1 << init_point - 1)
+        res.append(0)#
+        results.append([sum_path, res, current_points])
+
+    max_res = results[0]
+    for resul in results:
+        if resul[0] < max_res[0]:
+            max_res = resul
+    return max_res
 
 
 # -----------------------------------------------------------------
-def tsp_next(m, s, init_point):
+def tsp_next(m, s, init_point, current_points):
     if m[3][s][init_point] != -1:
         return m[3][s][init_point]
     if s == 0:
@@ -104,19 +113,19 @@ def tsp_next(m, s, init_point):
     for i in range(m[0]):
         index = 1 << i
         if s & index != 0:
-            if m[4] == 0 and points[i][2] == M_TYPE:
-                return INF
-            if m[5] == 0 and points[i][2] == N_TYPE:
-                return INF
-            if points[i][2] == M_TYPE:
+            if m[4] == 0 and current_points[i + 1][2] == M_TYPE:
+                continue
+            if m[5] == 0 and current_points[i + 1][2] == N_TYPE:
+                continue
+            if current_points[i + 1][2] == M_TYPE:
                 m[4] -= 1
             else:
                 m[5] -= 1
-            sum_temp = tsp_next(m, s ^ index, i) + m[1][i + 1][init_point + 1]
+            sum_temp = tsp_next(m, s ^ index, i,current_points) + m[1][i + 1][init_point + 1]
             if sum_temp < sum_path:
                 sum_path = sum_temp
                 m[2][s][init_point] = i + 1
-            if points[i][2] == M_TYPE:
+            if current_points[i + 1][2] == M_TYPE:
                 m[4] += 1
             else:
                 m[5] += 1
